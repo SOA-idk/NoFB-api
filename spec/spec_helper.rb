dc5fec353204b7779b9a356ff45616049e9e3b85
ginagigo123 <ginagigo123@gmail.com>
2021-10-24 16:15:11 +0800

    fix bug of cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,9 @@ require_relative '../lib/facebook_api'
 SimpleCov.start
 
 GROUP_ID = '302165911402681'
-CONFIG = YAML.safe_load(File.read('../config/secrets.yml'))
+CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
 ACCESS_TOKEN = CONFIG['FB_TOKEN']
-CORRECT = YAML.safe_load(File.read('./fixtures/nofb_results.yml'))
+CORRECT = YAML.safe_load(File.read('spec/fixtures/nofb_results.yml'))
 
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'Nofb_api'
