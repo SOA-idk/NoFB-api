d82bafb01aefa26b27c5894e7fcca878003ccbba
ginagig123 <ginagigo123@gmail.com>
2021-10-30 20:33:39 +0800

    add mappers

---
+++ lib/mappers/posts_mapper.rb
@@ -0,0 +1,62 @@
+module NoFB
+  module FB
+    # Data Mapper: FB -> Posts entity
+    class PostsMapper
+      def initialize(fb_token, gateway_class = FB::Api)
+        @token = fb_token
+        @gateway_class = gateway_class
+        @gateway = @gateway_class.new(@token)
+      end
+    end
+
+    # build a lot posts
+    def build_entities(project_data)
+      project_data = project_data['data']
+      posts = Array.new
+
+      project_data.each do |data|
+        posts << DataMapper.new(data, token, @gateway_class).build_entity
+      end
+
+      NoFB::Entity::Posts.new(
+        posts: posts,
+        size: posts.length,
+        post_list: posts.map(&:id)
+      )
+    end
+
+    # build only a post
+    def build_entity(data)
+      DataMapper.new(data, token, @gateway_class).build_entity
+    end
+
+    # Extracts entity specific elements from data structure
+    class DataMapper
+      def initialize(data, token, gateway_class)
+        @data = data
+      end
+
+      def build_entity
+        NoFB::Entity::Post.new(
+          updated_time: updated_time,
+          message: message,
+          id: id
+        )
+      end
+
+      def updated_time
+        return @data['updated_time']
+      end
+
+      def message
+        return @data['message']
+      end
+
+      def id
+        return @data['id']
+      end
+
+      def 
+    end
+  end
+end
