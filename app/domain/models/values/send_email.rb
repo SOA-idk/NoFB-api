85850e14227956a6a7b0350e1252a3fa32324e00
hhorace <hhoracehsu@gmail.com>
2021-11-28 13:04:10 +0800

    fix conflictsss

--- app/domain/models/values/send_email.rb
+++ app/domain/models/values/send_email.rb
@@ -10,11 +10,11 @@ module NoFB
         domain_name = ENV['MAILGUN_DOMAIN_NAME']
         api_key = ENV['MAILGUN_API_KEY']
         send_url = "https://api:#{api_key}@api.mailgun.net/v3/#{domain_name}/messages"
-        RestClient.post(send_url,
+        RestClient.post send_url,
                         from: "idk-nofb <admin@idk-nofb.com>",
                         to: 'hhoracehsu@gmail.com',
                         subject: 'Subscribed Words Notification',
-                        text: 'Testing some Mailgun awesomness!')
+                        text: 'Testing some Mailgun awesomness!'
       end
     end
   end
